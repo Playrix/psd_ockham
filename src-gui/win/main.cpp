2df96f5928a79b8afc153a8f043e7ec2b77e481f
Алексей Комаров <a.komarov@playrix.com>
2018-01-06 11:54:35 +0300

    Switch from unicode to multibyte strings

--- src-gui/win/main.cpp
+++ src-gui/win/main.cpp
@@ -12,8 +12,8 @@
 
 // Global Variables
 HINSTANCE hInst;
-WCHAR szTitle[MAX_LOADSTRING];
-WCHAR szWindowClass[MAX_LOADSTRING];
+CHAR szTitle[MAX_LOADSTRING];
+CHAR szWindowClass[MAX_LOADSTRING];
 HWND hVersionLabel;
 HWND hLogText;
 
@@ -38,8 +38,8 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
     UNREFERENCED_PARAMETER(hPrevInstance);
     UNREFERENCED_PARAMETER(lpCmdLine);
 
-    LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
-    LoadStringW(hInstance, IDC_PSD_OCKHAM_GUI, szWindowClass, MAX_LOADSTRING);
+    LoadString(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
+    LoadString(hInstance, IDC_PSD_OCKHAM_GUI, szWindowClass, MAX_LOADSTRING);
     OckhamRegisterClass(hInstance);
 
     if (!InitInstance(hInstance, nCmdShow))
@@ -60,7 +60,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 
 ATOM OckhamRegisterClass(HINSTANCE hInstance)
 {
-    WNDCLASSEXW wcex;
+    WNDCLASSEX wcex;
 
     wcex.cbSize = sizeof(WNDCLASSEX);
 
@@ -76,14 +76,14 @@ ATOM OckhamRegisterClass(HINSTANCE hInstance)
     wcex.lpszClassName  = szWindowClass;
     wcex.hIconSm        = wcex.hIcon;
 
-    return RegisterClassExW(&wcex);
+    return RegisterClassEx(&wcex);
 }
 
 BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
 {
     hInst = hInstance;
 
-    HWND hWnd = CreateWindowW(szWindowClass, szTitle,
+    HWND hWnd = CreateWindow(szWindowClass, szTitle,
         WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU | WS_MINIMIZEBOX | WS_THICKFRAME,
         CW_USEDEFAULT, 0, 400, 300, nullptr, nullptr, hInstance, nullptr);
 
@@ -97,25 +97,25 @@ BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
     HFONT defaultFont = (HFONT)GetStockObject(DEFAULT_GUI_FONT);
 
     hVersionLabel = CreateWindowEx(
-        0, L"EDIT",
+        0, "EDIT",
         NULL,
         WS_CHILD | WS_VISIBLE | ES_READONLY,
         0, 0, 0, 0,
         hWnd, (HMENU)ID_VERSION_LABEL, (HINSTANCE)GetWindowLong(hWnd, GWL_HINSTANCE), NULL);
     SendMessage(hVersionLabel, WM_SETFONT, (WPARAM)defaultFont, NULL);
-    WCHAR szVersion[MAX_LOADSTRING];
-    LoadStringW(hInstance, IDS_APP_VERSION, szVersion, MAX_LOADSTRING);
+    CHAR szVersion[MAX_LOADSTRING];
+    LoadString(hInstance, IDS_APP_VERSION, szVersion, MAX_LOADSTRING);
     SendMessage(hVersionLabel, WM_SETTEXT, FALSE, (LPARAM)szVersion);
 
     hLogText = CreateWindowEx(
-        WS_EX_STATICEDGE, L"EDIT",
+        WS_EX_STATICEDGE, "EDIT",
         NULL,
         WS_CHILD | WS_VISIBLE | ES_MULTILINE | WS_VSCROLL | ES_AUTOVSCROLL | ES_READONLY,
         0, 0, 0, 0,
         hWnd, (HMENU)ID_LOG_TEXT, (HINSTANCE)GetWindowLong(hWnd, GWL_HINSTANCE), NULL);
     SendMessage(hLogText, WM_SETFONT, (WPARAM)defaultFont, NULL);
-    WCHAR szDefaultText[MAX_LOADSTRING];
-    LoadStringW(hInstance, IDS_LOG_DEFAULT_TEXT, szDefaultText, MAX_LOADSTRING);
+    CHAR szDefaultText[MAX_LOADSTRING];
+    LoadString(hInstance, IDS_LOG_DEFAULT_TEXT, szDefaultText, MAX_LOADSTRING);
     SendMessage(hLogText, WM_SETTEXT, FALSE, (LPARAM)szDefaultText);
 
 
@@ -125,14 +125,14 @@ BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
     return TRUE;
 }
 
-static void AddToLogText(const WCHAR* text)
+static void AddToLogText(const CHAR* text)
 {
     int outLength = GetWindowTextLength(hLogText);
     SendMessage(hLogText, EM_SETSEL, outLength, outLength);
     SendMessage(hLogText, EM_REPLACESEL, FALSE, (LPARAM)text);
 }
 
-static void SetLogText(const WCHAR* text)
+static void SetLogText(const CHAR* text)
 {
     SendMessage(hLogText, WM_SETTEXT, FALSE, (LPARAM)text);
 }
@@ -185,24 +185,24 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
             HDROP hDrop = (HDROP)wParam;
 
             UINT count = DragQueryFile(hDrop, 0xFFFFFFFF, NULL, 0);
-            WCHAR buf[MAX_FILESTRING];
-            std::vector<std::basic_string<WCHAR>> files;
+            CHAR buf[MAX_FILESTRING];
+            std::vector<std::basic_string<CHAR>> files;
             for (UINT i = 0; i < count; ++i)
             {
-                if (DragQueryFile(hDrop, i, (LPWSTR)&buf, MAX_FILESTRING) != 0)
+                if (DragQueryFile(hDrop, i, (LPSTR)&buf, MAX_FILESTRING) != 0)
                 {
-                    files.push_back(std::basic_string<WCHAR>(buf));
+                    files.push_back(std::basic_string<CHAR>(buf));
                 }
             }
 
             DragFinish(hDrop);
 
-            SetLogText(L"");
+            SetLogText("");
 
             for (auto it: files)
             {
                 AddToLogText(it.c_str());
-                AddToLogText(L"\r\n");
+                AddToLogText("\r\n");
             }
         }
         break;
@@ -212,7 +212,7 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
             int y = border;
 
             SIZE sizeText;
-            WCHAR buf[MAX_LOADSTRING];
+            CHAR buf[MAX_LOADSTRING];
             GetWindowText(hVersionLabel, buf, MAX_LOADSTRING);
             HDC hDC = GetDC(NULL);
             HFONT childFont = (HFONT)SendMessage(hVersionLabel, WM_GETFONT, 0,0);
