d4c2fa102570ea799ebfac7078487a0c4a7f0d8c
Алексей Комаров <a.komarov@playrix.com>
2018-01-09 09:35:46 +0300

    Fix hardcoded path to cmake

--- make_win.bat
+++ make_win.bat
@@ -1,6 +1,6 @@
 @echo off
 
-set build_path="build/vs2017"
+set build_path="build/vs2015"
 set source_path="%CD%"
 
 if NOT EXIST %build_path% (
@@ -9,7 +9,7 @@ if NOT EXIST %build_path% (
 
 pushd %build_path%
 
-CALL m:\bin\cmake\bin\cmake.exe -G "Visual Studio 14 2015" %source_path%  || goto :do_exit
+CALL cmake.exe -G "Visual Studio 14 2015" %source_path%  || goto :do_exit
 
 popd
 
