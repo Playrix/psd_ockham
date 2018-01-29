c914dcd2222d497305e963c3f531e2f6779838e9
Алексей Комаров <a.komarov@playrix.com>
2018-01-29 15:30:59 +0300

    Increment versions

--- src/version.h
+++ src/version.h
@@ -19,6 +19,6 @@
 #ifndef __LIB_PSD_OCKHAM_VERSION_H__
 #define __LIB_PSD_OCKHAM_VERSION_H__
 
-const char* const PSD_OCKHAM_VERSION = "1.10";
+const char* const PSD_OCKHAM_VERSION = "1.11";
 
 #endif //ifndef __LIB_PSD_OCKHAM_VERSION_H__
