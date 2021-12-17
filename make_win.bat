b3429a09904bcaca10cbc66e58df2157fdbc5efc
Иван Могиш <32261095+immogish@users.noreply.github.com>
2021-12-17 10:17:14 +0300

    Change Windows builder to VS2019

--- make_win.bat
+++ make_win.bat
@@ -1,6 +1,6 @@
 @echo off
 
-set build_path="build/vs2017"
+set build_path="build/vs2019"
 set source_path="%CD%"
 
 if NOT EXIST %build_path% (
@@ -9,7 +9,7 @@ if NOT EXIST %build_path% (
 
 pushd %build_path%
 
-CALL cmake.exe -G "Visual Studio 15 2017" %source_path%  || goto :do_exit
+CALL cmake.exe -G "Visual Studio 16 2019" %source_path%  || goto :do_exit
 
 popd
 
